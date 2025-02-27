9914875b9d3fce01d4dfa457d519963f9f9f6c87
Jared Folkins <jfolkins@gmail.com>
2025-02-26 22:42:11 -0800

    enhc: update log

--- main.go
+++ main.go
@@ -278,7 +278,7 @@ func shellHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// LOG
-		logger.Printf("CACHED: %s\n%s\n", session, inputCmd)
+		//logger.Printf("CACHED: %s\n%s\n", session, inputCmd)
 
 		fmt.Fprintf(w, string(jsonResp))
 		return
